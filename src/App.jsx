0fadbfc583f3ff216b072a508529000b85b4f55c
chiffonkla <116045907+chiffonkla@users.noreply.github.com>
2025-11-06 20:13:46 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   )
