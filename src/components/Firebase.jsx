f45d344c83555d385cb4de97ce9fa4b10bf764a6
Atticus <144712135+atile4@users.noreply.github.com>
2025-11-07 14:51:27 -0800

    Update Firebase.jsx

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -6,13 +6,7 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVEm25iPBkBoq1JxkkPfzWmYLfhe3K0s0",
-  authDomain: "zotmarket-3e777.firebaseapp.com",
-  projectId: "zotmarket-3e777",
-  storageBucket: "zotmarket-3e777.appspot.com",
-  messagingSenderId: "445300335827",
-  appId: "1:445300335827:web:780cfc83959955c7622026",
-  measurementId: "G-6H0NRZD95K"
+
 };
 
 
