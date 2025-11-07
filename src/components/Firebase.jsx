a2b5bb6260a2c49bc8afb3823ae869ef49935c11
Atticus <144712135+atile4@users.noreply.github.com>
2025-11-06 22:58:26 -0800

    Update Firebase.jsx
    add firebaseConfig to be exported

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -1,20 +1,18 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBVEm25iPBkBoq1JxkkPfzWmYLfhe3K0s0",
   authDomain: "zotmarket-3e777.firebaseapp.com",
   projectId: "zotmarket-3e777",
   storageBucket: "zotmarket-3e777.appspot.com",
   messagingSenderId: "445300335827",
   appId: "1:445300335827:web:780cfc83959955c7622026",
-  measurementId: "G-6H0NRZD95K"
+  measurementId: "G-6H0NRZD95K",
 };
-
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
